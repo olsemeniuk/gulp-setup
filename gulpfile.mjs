704d686a2e0c4c92ab204033b58b11a2e549706c
olsemeniuk <ol.semeniuk.mail@gmail.com>
2024-03-31 15:32:07 -0700

    changed gulp version

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,82 +1,88 @@
 // import gulp
-import gulp from 'gulp';
-const { series, parallel, src, dest, watch } = gulp;
+import gulp from "gulp";
+const {series, parallel, src, dest, watch} = gulp;
 
 // import plugins
 // sass
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
 const sass = gulpSass(dartSass);
 // sourcemaps
-import sourcemaps from 'gulp-sourcemaps';
+import sourcemaps from "gulp-sourcemaps";
 // files concatenation and rename
-import concat from 'gulp-concat';
+import concat from "gulp-concat";
 // js minification
-import terser from 'gulp-terser';
+import terser from "gulp-terser";
 // browser live reload
-import browserSync from 'browser-sync';
+import browserSync from "browser-sync";
 const gulpBrowserSync = browserSync.create();
 // autoprefixer
-import autoprefixer from 'gulp-autoprefixer';
+import autoprefixer from "gulp-autoprefixer";
 // delete files
-import { deleteAsync } from 'del';
+import {deleteAsync} from "del";
 // pug
-import pug from 'gulp-pug';
+import pug from "gulp-pug";
+// images
+import imagemin from "gulp-imagemin";
 
 // paths
 const paths = {
     pug: {
-        src: './app/*.pug',
-        dest: './dist/'
+        src: "./app/*.pug",
+        dest: "./dist/"
     },
     styles: {
-        src: './app/sass/main.sass',
-        dest: './dist/css/'
+        src: "./app/sass/main.sass",
+        dest: "./dist/css/"
     },
     scripts: {
-        src: './app/js/**/*.js',
-        dest: './dist/js/'
+        src: "./app/js/**/*.js",
+        dest: "./dist/js/"
     },
     images: {
-        src: './app/images/**/*',
-        dest: './dist/images/'
+        src: "./app/images/**/*",
+        dest: "./dist/images/"
     }
 };
 
 // tasks
 function clean() {
-    return deleteAsync(['./dist/**/*']);
+    return deleteAsync(["./dist/**/*"]);
 }
 
 function styles() {
     return src(paths.styles.src)
         .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
         .pipe(autoprefixer())
-        .pipe(concat('style.min.css'))
-        .pipe(sourcemaps.write('./'))
+        .pipe(concat("style.min.css"))
+        .pipe(sourcemaps.write("./"))
         .pipe(dest(paths.styles.dest))
         .pipe(gulpBrowserSync.stream());
 }
 
 function scripts() {
     return src(paths.scripts.src)
-        .pipe(concat('main.min.js'))
+        .pipe(concat("main.min.js"))
         .pipe(sourcemaps.init())
         .pipe(terser())
-        .pipe(sourcemaps.write('./'))
+        .pipe(sourcemaps.write("./"))
         .pipe(dest(paths.scripts.dest))
         .pipe(gulpBrowserSync.stream());
 }
 
 function html() {
-    return src(paths.pug.src).pipe(pug()).pipe(dest(paths.pug.dest)).pipe(gulpBrowserSync.stream());
+    return src(paths.pug.src)
+        .pipe(pug())
+        .pipe(dest(paths.pug.dest))
+        .pipe(gulpBrowserSync.stream());
 }
+
 
 function watching() {
     gulpBrowserSync.init({
         server: {
-            baseDir: './dist'
+            baseDir: "./dist"
         }
     });
 
@@ -85,5 +91,5 @@ function watching() {
     watch(paths.pug.src, html);
 }
 
-export { clean };
+export {clean}
 export default series(clean, parallel(html, styles, scripts), watching);
