34826f7cb2a604c43ec6b5f3aca8bfccf2c9969d
Oleksandr Semeniuk <ol.semeniuk.mail@gmail.com>
2024-09-25 00:45:54 +0300

    added plugins for fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,39 @@
 // gulp
 const { src, dest, series, watch, parallel } = require("gulp");
 
-// plugins
-const sass = require("gulp-sass")(require("sass"));
+// general purpose plugins
 const sourcemaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
+
+/*
+ * ==================
+ * PUG START
+ * ==================
+ */
+// plugins
 const pug = require("gulp-pug");
-const terser = require("gulp-terser");
-const browserSync = require("browser-sync").create();
-const gulpAvif = require("gulp-avif");
-const newer = require("gulp-newer");
-const fonter = require("gulp-fonter");
-// import ttf2woff2 from 'gulp-ttf2woff2';
-
 // files
-const styleFiles = ["app/sass/styles.sass"];
 const pugFiles = ["app/*.pug"];
-const jsFiles = ["app/js/script.js"];
-const imagesFiles = ["app/images/**/*"];
-const imagesDest = "dist/images";
-const fontsFiles = 
-
 // tasks
 function managePugFiles() {
   return src(pugFiles).pipe(pug()).pipe(dest("dist"));
 }
+/*
+ * ==================
+ * PUG END
+ * ==================
+ */
 
+/*
+ * ==================
+ * STYLES START
+ * ==================
+ */
+// plugins
+const sass = require("gulp-sass")(require("sass"));
+// files
+const styleFiles = ["app/sass/styles.sass"];
+// tasks
 async function manageStyleFiles() {
   const autoprefixer = (await import("gulp-autoprefixer")).default;
   return src(styleFiles)
@@ -36,7 +44,22 @@ async function manageStyleFiles() {
     .pipe(sourcemaps.write("../maps"))
     .pipe(dest("dist/css"));
 }
+/*
+ * ==================
+ * STYLES END
+ * ==================
+ */
 
+/*
+ * ==================
+ * JS START
+ * ==================
+ */
+// plugins
+const terser = require("gulp-terser");
+// files
+const jsFiles = ["app/js/script.js"];
+// tasks
 function manageJSFiles() {
   return src(jsFiles)
     .pipe(sourcemaps.init())
@@ -45,7 +68,24 @@ function manageJSFiles() {
     .pipe(sourcemaps.write("../maps"))
     .pipe(dest("dist/js"));
 }
+/*
+ * ==================
+ * JS END
+ * ==================
+ */
 
+/*
+ * ==================
+ * IMAGES START
+ * ==================
+ */
+// plugins
+const gulpAvif = require("gulp-avif");
+const newer = require("gulp-newer");
+// files
+const imagesFiles = ["app/images/**/*"];
+const imagesDest = "dist/images";
+// tasks
 function imagesToAvif() {
   return src([...imagesFiles, "!app/images/**/*.svg"], {
     base: "app/images",
@@ -78,6 +118,52 @@ async function imagesImagemin() {
     .pipe(dest(imagesDest));
 }
 
+const manageImages = parallel(imagesToAvif, imagesToWebp, imagesImagemin);
+/*
+ * ==================
+ * IMAGES END
+ * ==================
+ */
+
+/*
+ * ==================
+ * FONTS START
+ * ==================
+ */
+// plugins
+const fonter = require("gulp-fonter");
+// files
+const fontsFiles = "app/fonts/**/*";
+const fontsDest = "dist/fonts";
+// tasks
+function fontsToWoff() {
+  return src(fontsFiles, { encoding: false, removeBOM: false })
+    .pipe(fonter({ formats: ["woff", "ttf"] }))
+    .pipe(dest(fontsDest));
+}
+
+async function fontsToWoff2() {
+  const ttf2woff2 = (await import("gulp-ttf2woff2")).default;
+  return src(`${fontsDest}/**/*.ttf`, { encoding: false, removeBOM: false })
+    .pipe(ttf2woff2())
+    .pipe(dest(fontsDest));
+}
+
+const manageFonts = series(fontsToWoff, fontsToWoff2);
+/*
+ * ==================
+ * FONTS END
+ * ==================
+ */
+
+/*
+ * ==================
+ * WATCHING START
+ * ==================
+ */
+// plugins
+const browserSync = require("browser-sync").create();
+// tasks
 function watching() {
   browserSync.init({
     server: {
@@ -88,13 +174,20 @@ function watching() {
   watch(pugFiles, managePugFiles);
   watch(styleFiles, manageStyleFiles);
   watch(jsFiles, manageJSFiles);
-  watch(imagesFiles, parallel(imagesToAvif, imagesToWebp, imagesImagemin));
+  watch(fontsFiles, manageFonts);
+  watch(imagesFiles, manageImages);
   watch("dist/**/*").on("change", browserSync.reload);
 }
+/*
+ * ==================
+ * WATCHING END
+ * ==================
+ */
 
+// clean dist folder
 async function cleanDist() {
   const { deleteAsync } = await import("del");
-  return deleteAsync(["dist/**/*"]);
+  return deleteAsync(["dist/**/*", "!dist/.gitkeep"]);
 }
 
 // exports
@@ -104,13 +197,15 @@ exports.build = series(
   managePugFiles,
   manageStyleFiles,
   manageJSFiles,
-  parallel(imagesToAvif, imagesToWebp, imagesImagemin)
+  manageFonts,
+  manageImages
 );
 exports.default = series(
   cleanDist,
   managePugFiles,
   manageStyleFiles,
   manageJSFiles,
-  parallel(imagesToAvif, imagesToWebp, imagesImagemin),
+  manageFonts,
+  manageImages,
   watching
 );
